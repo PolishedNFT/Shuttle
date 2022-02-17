edbd6b706f0244a3bcfe863c5fb1971e83916abb
schwarzjake <schwarz.jake@protonmail.com>
2022-02-16 18:07:59 -0700

    add commandline argument for import dir

--- src/main.js
+++ src/main.js
@@ -2,15 +2,16 @@ const { NFTStorage, File } = require('nft.storage');
 const mime = require('mime');
 const fs = require('fs');
 const path = require('path');
+const { performance } = require('perf_hooks');
 
 const baseUri = 'ipfs://';
 
 const NFT_STORAGE_KEY = '';
 
 async function fileFromPath(filePath) {
-	const content = await fs.promises.readFile(filePath)
-	const type = mime.getType(filePath)
-	return new File([content], path.basename(filePath), { type })
+	const content = await fs.promises.readFile(filePath);
+	const type = mime.getType(filePath);
+	return new File([content], path.basename(filePath), { type });
 }
 
 async function loadManifest(filePath) {
@@ -18,29 +19,35 @@ async function loadManifest(filePath) {
 	return JSON.parse(content);
 }
 
-function ensureFolderStructure() {
-	const buildDir = path.resolve(__dirname, '../build');
+function getImportDir() {
+	const arg = process.argv[2];
+	const lastChar = arg[arg.length - 1];
 
-	if (fs.existsSync(buildDir)) {
-		fs.rmSync(buildDir, { recursive: true });
+	if (lastChar === '/' && arg.length > 1) {
+		return arg.slice(0, arg.length - 1);
 	}
 
-	if (!fs.existsSync(buildDir)) {
-		fs.mkdirSync(buildDir);
-	}
-
-	if (!fs.existsSync(`${buildDir}/metadata`)) {
-		fs.mkdirSync(`${buildDir}/metadata`);
-	}
+	return arg;
 }
 
 async function main() {
-	ensureFolderStructure();
-
 	console.log('Shuttle [1.0.0]');
 	console.log('---------------------------------');
 
-	const manifest = await loadManifest(path.resolve(__dirname, '../import/manifest.json'));
+	if (process.argv.length !== 3) {
+		console.log('[?] Usage: npm run start /path/to/input/');
+		return;
+	}
+
+	const startTime = performance.now();
+
+	const importDir = getImportDir();
+
+	if (!fs.existsSync(`${importDir}/metadata`)) {
+		fs.mkdirSync(`${importDir}/metadata`);
+	}
+
+	const manifest = await loadManifest(`${importDir}/manifest.json`);
 	if (!manifest || !manifest.metadata) {
 		console.log('[!] Failed to load manifest file');
 		return;
@@ -56,8 +63,8 @@ async function main() {
 	const images = [];
 
 	for (const { tokenId } of manifest.metadata) {
-		images.push(await fileFromPath(`./import/images/${tokenId}.png`));
-		console.log(`[+] Loaded image ${tokenId}`);
+		images.push(await fileFromPath(`${importDir}/images/${tokenId}.png`));
+		console.log(`[+] Selected image ${tokenId}`);
 	}
 
 	console.log(`[@] Uploading images...`);
@@ -78,10 +85,10 @@ async function main() {
 		};
 
 		const serializedMetadata = JSON.stringify(manifest.metadata[i], null, 2);
-		const metadataPath = path.resolve(__dirname, `../build/metadata/${tokenId}`);
+		const metadataPath = `${importDir}/metadata/${tokenId}`;
 
 		fs.writeFileSync(metadataPath, serializedMetadata);
-		console.log(`[+] Saved metadata file for ${tokenId}`);
+		console.log(`[+] Created metadata file for ${tokenId}`);
 
 		metadatas.push(await fileFromPath(metadataPath));
 	}
@@ -92,15 +99,26 @@ async function main() {
 
 	console.log(`[=] Uploading metadata files complete | CID : ${metadataCid}`);
 
-	const serializedManifest = JSON.stringify(manifest, null, 2);
-	fs.writeFileSync(path.resolve(__dirname, '../import/manifest.json'), serializedManifest);
+	const newManifest = {
+		name: manifest.name,
+		description: manifest.description,
+		total: manifest.total,
+		provenanceHash: manifest.provenanceHash,
+		baseUri: `${baseUri}${metadataCid}/`,
+		metadata: manifest.metadata,
+	};
+
+	const serializedManifest = JSON.stringify(newManifest, null, 2);
+	fs.writeFileSync(`${importDir}/manifest.json`, serializedManifest);
 
 	console.log('[=] Updated manifest file.');
 
+	const endTime = (Math.abs(performance.now() - startTime) / 1000).toFixed(4);
+
 	console.log('---------------------------------');
-	console.log(`[$] Successful Shuttled ${manifest.metadata.length} NFTs`);
-	console.log(`[#] Images: ipfs://${imagesCid} | https://ipfs.io/ipfs/${imagesCid}`);
-	console.log(`[#] Metadatas: ipfs://${metadataCid} | https://ipfs.io/ipfs/${metadataCid}`);
+	console.log(`[$] Successful Shuttled ${manifest.metadata.length} NFTs in ${endTime}s`);
+	console.log(`[#] Image: ${baseUri}${imagesCid} | https://ipfs.io/ipfs/${imagesCid}`);
+	console.log(`[#] Metadata: ${manifest.baseUri} | https://ipfs.io/ipfs/${metadataCid}`);
 }
 
 main().catch(err => {
